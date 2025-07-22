0877003e196816a7511aa08ad0c8fb207b7942f8
Israel <israel.oliversw@gmail.com>
2025-07-22 17:15:04 -0300

    Adicionadno alagumas coisas

---
+++ src/main/java/net/playtosmiles/tutorialmods/item/ModItens.java
@@ -0,0 +1,9 @@
+package net.playtosmiles.tutorialmods.item;
+
+import net.playtosmiles.tutorialmods.TutorialMod;
+
+public class ModItens {
+    public static void registerModItens() {
+        TutorialMod.LOGGER.info("Registrando item do mod para " + TutorialMod.MOD_ID);
+    }
+}
