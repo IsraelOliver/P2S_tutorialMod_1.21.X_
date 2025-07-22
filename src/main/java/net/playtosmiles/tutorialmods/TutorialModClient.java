4afc1de840375c564c19272232ea9a29d9bda10e
Israel <israel.oliversw@gmail.com>
2025-07-22 15:08:51 -0300

    Initial commit

---
+++ src/main/java/net/playtosmiles/tutorialmods/TutorialModClient.java
@@ -0,0 +1,11 @@
+package net.playtosmiles.tutorialmods;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
